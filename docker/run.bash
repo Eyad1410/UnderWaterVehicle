85d69b16eae4da3c437300d8e2f474365f6b6336
apola <yan_1whun1@hotmail.com>
2024-10-29 13:04:14 +0400

    decouple dev

--- docker/run.bash
+++ docker/run.bash
@@ -5,12 +5,12 @@ xhost +local:docker
 docker run -it\
   --privileged \
   --network=host \
-  --env="ROS_DOMAIN_ID=47" \
+  --env="ROS_DOMAIN_ID=27" \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="/dev:/dev" \
   --volume="/run/user:/run/user" \
   --volume="${HOME}/UnderWaterVehicle:/UnderWaterVehicle" \
-  --name="bluerov2-simulator" \
+  --name="bluerov2-dev" \
   hub.ci.dfl.ae/roboticslab/ros2_humble_x86_no_gpu:bluerov2
