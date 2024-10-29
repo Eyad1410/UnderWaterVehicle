a96763b4e7c9ac89e6648550a6abdcdcb821449d
apola <yan_1whun1@hotmail.com>
2024-10-29 14:30:31 +0400

    update

--- docker/run.bash
+++ docker/run.bash
@@ -8,9 +8,11 @@ docker run -it\
   --env="ROS_DOMAIN_ID=27" \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
+  --env="FASTRTPS_DEFAULT_PROFILES_FILE=/home/bluerov2/rtps_udp_profile.xml" \
+  --env="RMW_FASTRTPS_USE_QOS_FROM_XML=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="/dev:/dev" \
   --volume="/run/user:/run/user" \
   --volume="${HOME}/UnderWaterVehicle:/UnderWaterVehicle" \
-  --name="bluerov2-dev" \
+  --name="uuv_ardusub_dev" \
   hub.ci.dfl.ae/roboticslab/ros2_humble_x86_no_gpu:bluerov2
